be1b98cf76d10c06287e5107b382f60502c06796
Joswin_Shogi <104709945+joswinshogi@users.noreply.github.com>
2023-12-20 01:05:25 +0530

    Navbar-updated

--- ecommerce/src/Components/Navbar/index.js
+++ ecommerce/src/Components/Navbar/index.js
@@ -1,11 +1,94 @@
 'use client'
 
-import React from 'react'
+
+const isadminview = false
+const isAuthuser = false
+const user = {
+    role: 'Admin'
+}
+
+
+function NavItems() {
+    return (
+        <div className=' items-center justify-between w-full md:flex md:w-auto' id='nav-items'>
+            <ul className=' flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:border-0 bg-white'>
+                {
+                    isadminview ? adminNavOptions.map(item => <li className=' cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0' key={item.id}>
+                        {item.label}
+
+
+                    </li>) : navOptions.map(item => <li className=' cursor-pointer block py-2 pl-3 pr-4 text-gray-900 rounded md:p-0' key={item.id}>
+                        {item.label}
+
+
+                    </li>)
+                }
+
+            </ul>
+
+        </div>
+    )
+}
+
+import { adminNavOptions, navOptions, styles } from '@/utils'
+import React, { Fragment } from 'react'
 
 const Navbar = () => {
-  return (
-    <div className=' bg-gray-300 text-black'>Navbar</div>
-  )
+    return <>
+        <nav className=' bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200'>
+            <div className=' max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+                <div className=' flex items-center cursor-pointer'>
+                    <span className=' self-center text-2xl font-semibold whitespace-nowrap text-black'>Ecommercery</span>
+
+                </div>
+                <div className=' flex md:order-2 gap-2 text-black'>
+                    {
+                        !isadminview && isAuthuser ?
+                            <Fragment>
+                                <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Account</button>
+                                <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Cart</button>
+                            </Fragment> : null
+                    }
+                    {
+                        user?.role === 'Admin' ?
+                            isadminview ? <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Client view</button> : <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Admin view</button>
+                            : null
+                    }
+                    {
+                        isAuthuser ? <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Logout</button> : <button className='mt-1.5 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'>Login</button>
+                    }
+                    <button
+                        data-collapse-toggle="navbar-sticky"
+                        type="button"
+                        className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-sticky"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-6 h-6"
+                            aria-hidden="true"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+
+                </div>
+                <NavItems />
+
+            </div>
+
+        </nav>
+
+
+    </>
 }
 
 export default Navbar
